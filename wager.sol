56a845ca886fc4e27f96ac2e8c1a594f8ea2f07f
Trevor Stenson <stenson124@gmail.com>
2017-08-22 01:17:52 -0700

    Create wager.sol
    wager.sol is a simple king-of-the-hill type betting smart contract, where players have the chance to win a pot of ether by outbidding other players. Each time a new bet is recorded, the remaining game time increases by one hour. The pot winner receives 90% of the prize, the contract creator receives 5%, and the remaining 5% is used as the starting pot of the next game.

---
+++ wager.sol
@@ -0,0 +1,104 @@
+pragma solidity ^0.4.0;
+
+contract wager {
+
+    address creator;
+    uint gameStart;
+    uint gameEnd;
+    uint interval;
+    uint highestBid;
+    uint currentPot;
+    uint remainingTime;
+    address highestBidder;
+    struct Entry {
+    address creator;
+    uint balance;
+    }
+
+    modifier onlyowner {
+        if (msg.sender == creator) {
+            _;
+        }
+    }
+
+    event Bet(address indexed _from, uint _value);
+
+    event Time(uint _left);
+
+    function wager(){
+        highestBid = 0;
+        highestBidder = msg.sender;
+        currentPot = this.balance;
+        interval = 1 hours;
+        creator = msg.sender;
+        gameStart = now + 30;
+        startGame(gameStart, 0);
+    }
+
+    function newBet() payable {
+        Bet(msg.sender, msg.value);
+        if (msg.value > highestBid) {
+            highestBid = msg.value;
+            highestBidder = msg.sender;
+            currentPot += msg.value;
+            gameEnd += interval;
+        }
+    }
+
+    function startGame(uint start, uint pot) {
+        gameStart = start;
+        gameEnd = gameStart + interval;
+        currentPot = pot;
+        highestBid = 0;
+        highestBidder = creator;
+    }
+
+    function getRemaining() constant returns(uint) {
+        remainingTime = gameEnd - now;
+        Time(remainingTime);
+        return remainingTime;
+        Time(remainingTime);
+    }
+
+    function gameOver() constant returns(bool) {
+        if (gameEnd <= now) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function resetGame() onlyowner {
+        require(gameOver());
+        uint ownerShare = (this.balance * 5) / 100;
+        uint winnerShare = (this.balance * 9) / 100;
+        uint nextPot = (this.balance * 5) /  100;
+
+        highestBidder.transfer(winnerShare);
+        creator.transfer(ownerShare);
+        Time(remainingTime);
+        startGame(gameEnd + 30, nextPot);
+    }
+
+    function getCurrentKing() constant returns(address){
+        return highestBidder;
+    }
+
+    function getCurrentBid() constant returns (uint){
+        return highestBid;
+    }
+
+    function getCurrentPot() constant returns (uint){
+        return currentPot;
+    }
+
+    function getEndTime() returns (uint){
+        return (gameEnd - gameStart);
+    }
+
+    function kill() {
+        require(msg.sender == creator);
+        selfdestruct(creator);
+    }
+
+}
